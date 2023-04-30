833f93fcbe93b3933e7fb465713ea376bfa230c9
kjin <pkjin.eng@gmail.com>
2023-04-30 19:04:15 +0900

    POST 메서드 수정

--- app.ts
+++ app.ts
@@ -52,6 +52,7 @@ app.post("/today", async (req, res) => {
     time: req.body.time,
     name: req.body.name,
   };
+  res.send(list);
 });
 
 app.get("/yesterday", async (req, res) => {
@@ -80,6 +81,31 @@ app.post("/yesterday", async (req, res) => {
     time: req.body.time,
     name: req.body.name,
   };
+  res.send(list);
+});
+
+app.get("/year", async (req, res) => {
+  const getAPI = async (req: Request) => {
+    const API_URL = `http://openAPI.seoul.go.kr:8088/${API_KEY}/json/YearlyAverageAirQuality/1/100/2022`;
+    let response;
+    try {
+      response = await axios.get(API_URL);
+    } catch (error) {
+      console.log(error);
+    }
+    return response;
+  };
+  await getAPI(req).then((response) => {
+    res.json(response.data);
+  });
+});
+
+app.post("/year", async (req, res) => {
+  list = {
+    time: req.body.time,
+    name: req.body.name,
+  };
+  res.send(list);
 });
 
 app.listen(port, () => console.log("running"));
